00b0d28f701a6f319eb75b050d2e41f5ba2b855f
zhengsonglan <zhengsonglan@inspiry.cn>
2018-05-26 22:22:11 +0800

    1. 增加气温和天气描述。

--- lib/weather/home.dart
+++ lib/weather/home.dart
@@ -11,18 +11,36 @@ class WeatherHomeState extends State<WeaterHome> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title:'WeaterHome',
+      title: 'WeaterHome',
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text("Weather Home Page"),
           centerTitle: true,
         ),
-        body: new Center(
-          child: new Text("Hello World"),
-        ),
+        body: homeBody(),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
 
+  ///homeBody
+  Widget homeBody() {
+    return new Container(
+      alignment: Alignment.topCenter,
+      child: new Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text(
+            "12°",
+            style: new TextStyle(fontSize: 48.0),
+          ),
+          new Text(
+            "晴天",
+            style: new TextStyle(fontSize: 16.0, color: Colors.black38),
+          )
+        ],
+      ),
+    );
+  }
 }
