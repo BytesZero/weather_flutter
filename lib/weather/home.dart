c2955bdcc9298fcad0bca5c3486db88a49d35b84
zhengsonglan <zhengsonglan@inspiry.cn>
2018-05-27 11:27:17 +0800

    1. 天气描述英文转换成中文。 2. 根据天气描述动态改变背景图片。

--- lib/weather/home.dart
+++ lib/weather/home.dart
@@ -11,8 +11,24 @@ class WeaterHome extends StatefulWidget {
 
 ///绘制Weather页
 class WeatherHomeState extends State<WeaterHome> {
+  ///温度
   var temp = 0;
-  var weather = "null";
+
+  ///天气描述
+  var weather = '数据获取中...';
+
+  ///天气描述对应的背景图片
+  var weatherImage = 'res/backgrounds/sunny-bg.webp';
+
+  ///天气转换的映射
+  var weatherMap = {
+    'sunny': '晴天',
+    'cloudy': '多云',
+    'overcast': '阴',
+    'lightrain': '小雨',
+    'heavyrain': '大雨',
+    'snow': '雪'
+  };
 
   @override
   void initState() {
@@ -43,22 +59,34 @@ class WeatherHomeState extends State<WeaterHome> {
   ///homeBody
   Widget homeBody() {
     return new Container(
+      ///顶部居中对齐
       alignment: Alignment.topCenter,
+
+      ///装饰背景图片
       decoration: new BoxDecoration(
         image: new DecorationImage(
           alignment: Alignment.topCenter,
-          image: new AssetImage('res/backgrounds/sunny-bg.webp'),
+          image: new AssetImage(weatherImage),
         ),
       ),
       padding: new EdgeInsets.only(top: 140.0),
+
+      ///child 距离顶部140
       child: new Column(
+        ///最小大小，根据children计算
         mainAxisSize: MainAxisSize.min,
+
+        ///主体居中
         mainAxisAlignment: MainAxisAlignment.center,
+
         children: <Widget>[
+          ///温度
           new Text(
             "$temp°",
             style: new TextStyle(fontSize: 64.0),
           ),
+
+          ///天气描述
           new Text(
             weather,
             style: new TextStyle(fontSize: 18.0, color: Colors.black38),
@@ -70,7 +98,8 @@ class WeatherHomeState extends State<WeaterHome> {
 
   ///通过网络获取天气
   void getWeather() async {
-    var httpClient = new HttpClient(); //创建client
+    ///创建client
+    var httpClient = new HttpClient();
     var uri =
         Uri.parse('https://test-miniprogram.com/api/weather/now?city=北京市');
     var request = await httpClient.getUrl(uri);
@@ -81,7 +110,9 @@ class WeatherHomeState extends State<WeaterHome> {
       var date = JSON.decode(responseBody);
       setState(() {
         temp = date['result']['now']['temp'];
-        weather = date['result']['now']['weather'];
+        String tempWeather = date['result']['now']['weather'];
+        weatherImage = 'res/backgrounds/$tempWeather-bg.webp';
+        weather = weatherMap[tempWeather];
       });
     }
   }
